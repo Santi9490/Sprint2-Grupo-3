767b8d762ba6d4ec44df596b534c956ff07277e7
Santi9490 <m.prietoh@uniandes.edu.co>
2024-10-08 22:46:47 -0500

    cambios

--- ofipensiones/views.py
+++ ofipensiones/views.py
@@ -1,4 +1,8 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+def health_check(request):
+    return HttpResponse('ok')
