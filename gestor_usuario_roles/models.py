d6c59c46f0792bafcdcd744b0d498f7e22f69d10
Santi9490 <m.prietoh@uniandes.edu.co>
2024-10-08 23:10:06 -0500

    borro atributo

--- gestor_usuario_roles/models.py
+++ gestor_usuario_roles/models.py
@@ -4,8 +4,7 @@ class Estudiante(models.Model):
     nombre = models.CharField(max_length=50)
     apellido = models.CharField(max_length=50)
     codigo = models.CharField(max_length=20, default='0000')  
-    numero_acudiente = models.CharField(max_length=15, default='0000000000')  
-    fecha_nacimiento = models.DateField()
+    numero_acudiente = models.CharField(max_length=15, default='0000000000')
     direccion = models.CharField(max_length=100, default='Sin Dirección')  
 
     def __str__(self):
