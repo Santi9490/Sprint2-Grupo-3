1fc487c49c75791c25b914f6904575aaa40a149a
Santi9490 <m.prietoh@uniandes.edu.co>
2024-10-18 20:40:36 -0500

    cambios

--- gestor_usuario_roles/views.py
+++ gestor_usuario_roles/views.py
@@ -8,17 +8,16 @@ from concurrent.futures import ThreadPoolExecutor
 
 # Función para listar estudiantes
 def estudiante_list(request):
-    estudiantes = []
+    from django.shortcuts import render
+from django.contrib import messages
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from .forms import EstudianteForm
+from .models import Estudiante
 
-
-    with ThreadPoolExecutor(max_workers=175) as executor:
-        futures = [executor.submit(Estudiante.objects.get, id=est.id) for est in Estudiante.objects.all()]
-        for future in futures:
-            try:
-                estudiantes.append(future.result())
-            except Exception as e:
-                print(f"Error obteniendo estudiante: {e}")
-
+# Función para listar estudiantes
+def estudiante_list(request):
+    estudiantes = Estudiante.objects.all()
     context = {
         'estudiante_list': estudiantes
     }
@@ -29,7 +28,7 @@ def estudiante_create(request):
     if request.method == 'POST':
         form = EstudianteForm(request.POST)
         if form.is_valid():
-            form.save()
+            form.save() 
             messages.add_message(request, messages.SUCCESS, 'Estudiante creado exitosamente')
             return HttpResponseRedirect(reverse('estudianteCreate'))
         else:
