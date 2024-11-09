f319b879e33e15a86acd486cf6c2b70de3fb5261
Santi9490 <m.prietoh@uniandes.edu.co>
2024-11-08 20:50:09 -0500

    cambios urls

--- cuenta/urls.py
+++ cuenta/urls.py
@@ -3,8 +3,8 @@ from django.views.decorators.csrf import csrf_exempt
 from . import views
 
 urlpatterns = [
-    path('cuentas/', views.cuenta_list, name='CuentaList'),
-    path('cuenta/create/', views.cuenta_create, name='CuentaCreate'),
-    path('cuenta/<str:codigo>/', views.cuenta_detalle, name='CuentaDetalle'),
+    path('cuentas/', views.cuenta_list, name='cuenta_list'),
+    path('cuenta/create/', views.cuenta_create, name='cuenta_create'),
+    path('cuenta/<str:codigo>/', views.cuenta_detalle, name='cuenta_detalle'),
     path('health/', views.health_check, name='health_check'),  # Health Check
 ]
