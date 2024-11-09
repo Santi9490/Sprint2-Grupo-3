27d40a0917343cdb1b0cf69b75befbc9a1babf84
Santi9490 <m.prietoh@uniandes.edu.co>
2024-11-08 20:54:53 -0500

    cambios

--- cuenta/urls.py
+++ cuenta/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('cuentas/', views.cuenta_list, name='cuenta_list'),
-    path('cuenta/create/', views.cuenta_create, name='cuenta_create'),
+    path('cuentacreate/', views.cuenta_create, name='cuenta_create'),
     path('cuenta/<str:codigo>/', views.cuenta_detalle, name='cuenta_detalle'),
     path('health/', views.health_check, name='health_check'),  # Health Check
 ]
