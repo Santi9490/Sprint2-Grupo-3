6ce2012ef0e52dd70901d4026e733fd5c43b4fdb
Santi9490 <m.prietoh@uniandes.edu.co>
2024-11-08 19:20:02 -0500

    cambios

--- alarms/models.py
+++ alarms/models.py
@@ -1,15 +1,16 @@
 from django.db import models
-from gestor_usuario_roles.models import Estudiante, EstadoCuenta
+from gestor_usuario_roles.models import Estudiante
+from cuenta.models import Cuenta
 
 class Alarm(models.Model):
     estudiante = models.ForeignKey(Estudiante, on_delete=models.CASCADE, default=None)
-    cuenta = models.ForeignKey(EstadoCuenta, on_delete=models.CASCADE, default=None)
+    cuenta = models.ForeignKey(Cuenta, on_delete=models.CASCADE, default=None)
     pas_y_salvo = models.FloatField(null=True, blank=True, default=None)
     limitExceeded = models.FloatField(null=True, blank=True, default=None)
     dateTime = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return '{"estudiante": %s, "estado_cuenta": %s, "limitExceeded": %s, "dateTime": %s}' % (self.estudiante.name, self.estadocuenta.pas_y_salvo, self.limitExceeded, self.dateTime)
+        return '{"estudiante": %s, "estado_cuenta": %s, "limitExceeded": %s, "dateTime": %s}' % (self.estudiante.name, self.cuenta.pas_y_salvo, self.limitExceeded, self.dateTime)
     
     def toJson(self):
         alarm = {
