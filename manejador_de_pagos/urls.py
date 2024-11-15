1e55920804a7d587f21eb6c95f5001a08b543e83
Santi9490 <m.prietoh@uniandes.edu.co>
2024-11-14 21:13:45 -0500

    cambios urls

--- manejador_de_pagos/urls.py
+++ manejador_de_pagos/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('pagoCreate/', views.pago_create, name='pagos_create'),  
+    path('pagocreate/', views.pago_create, name='pagos_create'),  
     path('pagos/', views.pago_list, name='pago_list'),            
     path('pagos/<str:codigo>/', views.pago_list, name='pago_list_estudiante'),  
 ]
