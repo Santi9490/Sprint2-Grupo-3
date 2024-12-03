80ee7f45ae5486ecf7b91266de9f36d9078156f8
Santi9490 <m.prietoh@uniandes.edu.co>
2024-12-02 19:35:08 -0500

    creo carpetas xd

--- gestor_colegios/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class GestorColegiosConfig(AppConfig):
-    name = 'gestor_colegios'
--- gestor_colegios/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from django.views.decorators.csrf import csrf_exempt
-from . import views
-
-urlpatterns = [
-    path('colegios/', views.colegio_list, name='colegios'),
-    path('colegiocreate/', csrf_exempt(views.colegio_create), name='colegioCreate'),
-]
