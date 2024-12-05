9950336d8c3aa163256f70a925e76107c199e663
Santi9490 <m.prietoh@uniandes.edu.co>
2024-12-05 07:15:57 -0500

    confi

--- manejador_estudiantes/ofipensiones/auth0backend.py
+++ manejador_estudiantes/ofipensiones/auth0backend.py
@@ -6,12 +6,16 @@ class Auth0(BaseOAuth2):
     name = 'auth0' 
     SCOPE_SEPARATOR = ' ' 
     ACCESS_TOKEN_METHOD = 'POST' 
-    EXTRA_DATA = [ ('picture', 'picture') ] 
+    EXTRA_DATA = [ 
+        ('picture', 
+         'picture') 
+        ] 
     
     def authorization_url(self): 
         """Return the authorization endpoint.""" 
     
         return "https://" + self.setting('DOMAIN') + "/authorize"
+    
     def access_token_url(self): 
         """Return the token endpoint.""" 
         return "https://" + self.setting('DOMAIN') + "/oauth/token" 
@@ -19,11 +23,13 @@ class Auth0(BaseOAuth2):
     def get_user_id(self, details, response): 
         """Return current user id.""" 
         return details['user_id'] 
+    
     def get_user_details(self, response): 
         url = 'https://' + self.setting('DOMAIN') + '/userinfo' 
         headers = {'authorization': 'Bearer ' + response['access_token']} 
         resp = requests.get(url, headers=headers) 
         userinfo = resp.json() 
+        
         return {'username': userinfo['nickname'], 
                 'first_name': userinfo['name'], 
                 'picture': userinfo['picture'], 
@@ -35,8 +41,7 @@ class Auth0(BaseOAuth2):
 def getRole(request):
     user = request.user 
     auth0user = user.social_auth.filter(provider="auth0")[0]
-
-    accessToken = auth0user.extra_data['access_token'] 
+    accessToken = auth0user.extra_data['access_token']
     url = "https://dev-2lbfseybkx5rveyk.us.auth0.com/userinfo" 
     headers = {'authorization': 'Bearer ' + accessToken}
 
