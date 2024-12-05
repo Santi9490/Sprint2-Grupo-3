ba325df8a1f7c0caa2fa0b09d0ad17c461d0d633
Santi9490 <m.prietoh@uniandes.edu.co>
2024-12-04 20:20:03 -0500

    cambios

--- manejador_estudiantes/ofipensiones/settings.py
+++ manejador_estudiantes/ofipensiones/settings.py
@@ -33,7 +33,9 @@ ALLOWED_HOSTS = ["*"]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
+    'django.contrib.auth',
     'django.contrib.contenttypes',
+    'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'gestor_usuario_roles',
@@ -42,12 +44,13 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
 
 ROOT_URLCONF = 'ofipensiones.urls'
 
