bca7d8e2db404a6bd98abffcc8213675fe41aac1
Santi9490 <m.prietoh@uniandes.edu.co>
2024-12-04 22:29:25 -0500

    cambio

--- manejador_integraciones_contables/consultor_base_datos/urls.py
+++ manejador_integraciones_contables/consultor_base_datos/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('/generar_reporte/', views.generar_reporte, name='api_reportes')
+    path('generar_reporte/', views.generar_reporte, name='api_reportes')
 ]
--- manejador_integraciones_contables/manejador_de_pagos/urls.py
+++ manejador_integraciones_contables/manejador_de_pagos/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('pagos/', views.pago_list, name='pago_list'), 
     path('pagos/<int:id>/', views.pago_list, name='pago_list_by_id'),
     path('api/cuentas/<int:estudiante_id>/', views.obtener_cuentas_por_estudiante, name='api_cuentas_por_estudiante'),
-    path('/reporte/', views.generar_reporte_pdf, name='reportes')
+    path('reporte/', views.generar_reporte_pdf, name='reportes')
 ]
 
