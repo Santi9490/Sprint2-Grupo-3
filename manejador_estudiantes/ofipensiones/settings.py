690df412bc290c3fdcfaeb636b722825c13a4e05
Santi9490 <m.prietoh@uniandes.edu.co>
2024-12-04 19:47:28 -0500

    cambio ip

--- manejador_estudiantes/ofipensiones/settings.py
+++ manejador_estudiantes/ofipensiones/settings.py
@@ -136,7 +136,7 @@ MONGO_CLI = "mongodb://estudiante_user:isis2503@10.128.0.82:27017"
 
 LOGIN_URL = "/login/auth0"
 LOGIN_REDIRECT_URL = "/"
-LOGOUT_REDIRECT_URL = "https://dev-2lbfseybkx5rveyk.us.auth0.com/v2/logout?returnTo=http%3A%2F%2F35.238.151.115:8080"
+LOGOUT_REDIRECT_URL = "https://dev-2lbfseybkx5rveyk.us.auth0.com/v2/logout?returnTo=http%3A%2F%2F34.172.228.232:8080"
 
 SOCIAL_AUTH_TRAILING_SLASH = False  # Remove end slash from routes
 SOCIAL_AUTH_AUTH0_DOMAIN = 'dev-2lbfseybkx5rveyk.us.auth0.com'
