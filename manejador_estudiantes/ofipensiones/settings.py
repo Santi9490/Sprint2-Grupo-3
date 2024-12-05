8bb998f27ccd9d0641dd9d51b0ed4d94856a281d
Santi9490 <m.prietoh@uniandes.edu.co>
2024-12-04 19:44:44 -0500

    cambios

--- manejador_estudiantes/ofipensiones/settings.py
+++ manejador_estudiantes/ofipensiones/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 # MongoDB Atlas Config
-MONGO_CLI = os.environ['MONGO_CLI']
+MONGO_CLI = "mongodb://estudiante_user:isis2503@10.128.0.82:27017"
 
 
 # Default primary key field type
