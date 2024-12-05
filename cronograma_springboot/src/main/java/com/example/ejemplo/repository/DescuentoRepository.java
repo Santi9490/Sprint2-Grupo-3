c2b2d68e51dbd55ac4cb98228fbfecda1451311d
Santi9490 <m.prietoh@uniandes.edu.co>
2024-12-04 23:51:04 -0500

    cambios para que sirva todo

--- cronograma_springboot/src/main/java/com/example/ejemplo/repository/DescuentoRepository.java
+++ cronograma_springboot/src/main/java/com/example/ejemplo/repository/DescuentoRepository.java
@@ -2,10 +2,13 @@ package com.example.ejemplo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.example.ejemplo.entities.DescuentoEntitity;
 import org.springframework.stereotype.Repository;
 
+import com.example.ejemplo.entities.DescuentoEntity;
+
+
+
 @Repository
-public interface DescuentoRepository extends JpaRepository<DescuentoEntitity, Long>{
+public interface DescuentoRepository extends JpaRepository<DescuentoEntity, Long>{
 
 }
--- cronograma_springboot/src/main/java/com/example/ejemplo/repository/EventosRepository.java
+++ cronograma_springboot/src/main/java/com/example/ejemplo/repository/EventosRepository.java
@@ -2,11 +2,12 @@ package com.example.ejemplo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.example.ejemplo.entities.EventosEntitity;
 
 import org.springframework.stereotype.Repository;
 
+import com.example.ejemplo.entities.EventosEntity;
+
 @Repository
-public interface EventosRepository extends JpaRepository<EventosEntitity, Long>  {
+public interface EventosRepository extends JpaRepository<EventosEntity, Long>  {
 
 }
