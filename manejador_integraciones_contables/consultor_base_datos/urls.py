62f0cde185a14595833ae89ab73be0265df67559
Santi9490 <m.prietoh@uniandes.edu.co>
2024-12-05 09:11:49 -0500

    cambios

--- manejador_integraciones_contables/consultor_base_datos/urls.py
+++ manejador_integraciones_contables/consultor_base_datos/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('generar_reporte/', views.generar_reporte, name='api_reportes')
+    path('generar_reporte/', views.generar_reporte, name='api_reportes'),
+    path('actualizarCache/', views.actualizar_reporte_cache, name='api_actualizar_cache'),
 ]
